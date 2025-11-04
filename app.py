fe36831c7b4b0b4b6f4c61f5c0ecd7fb257e557c
X3NOM1 <kyaw1031999@gmail.com>
2025-11-05 05:30:45 +0630

    Update app.py

--- app.py
+++ app.py
@@ -8,15 +8,12 @@ app = Flask(__name__)
 def proxy(path):
     url = request.args.get("url")
     if not url:
-        return "Please provide ?url=<target>", 400
-    try:
-        resp = requests.get(url)
-        excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
-        headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
-        response = Response(resp.content, resp.status_code, headers)
-        return response
-    except Exception as e:
-        return str(e), 500
+        return "Please provide ?url=", 400
+    resp = requests.get(url)
+    excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
+    headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
+    response = Response(resp.content, resp.status_code, headers)
+    return response
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
